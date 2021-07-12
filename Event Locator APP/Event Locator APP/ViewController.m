739eba73a51118c467a52f97f91c3687070fc22a
Abenezer Molla <abenezer@minerva.kgi.edu>
2021-07-12 14:21:57 -0700

    Initializing the Project. Blank Screen.

---
+++ Event Locator APP/Event Locator APP/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Event Locator APP
+//
+//  Created by abenezermolla on 7/12/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
